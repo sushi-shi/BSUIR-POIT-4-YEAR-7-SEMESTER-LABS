8c7056c2257eb0b222470652d4f83768a993a9ec
Iwan Waitusenok <sleepplease@protonmail.ch>
2021-10-21 03:03:37 +0300

    ZOS3

--- ZOS/lab3-2/src/main.rs
+++ ZOS/lab3-2/src/main.rs
@@ -29,36 +29,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let reader = hound::WavReader::open(path)?;
     let spec = reader.spec();
     let len = reader.len() as usize;
+    let sample_rate = 5 * spec.sample_rate as usize;
 
     let mut samples = reader
         .into_samples::<i16>()
         .collect::<Result<Vec<_>, _>>()?
         .into_iter()
         .map(|i| Complex::new(i as f32, 0f32))
-        .take(len - len % spec.sample_rate as usize)
+        .take(len - len % sample_rate)
         .collect::<Vec<_>>();
 
     let mut planner = FftPlanner::<f32>::new();
-    let fft = planner.plan_fft_forward(spec.sample_rate as usize);
+    let fft = planner.plan_fft_forward(sample_rate);
     fft.process(&mut samples);
 
     let mut samples = samples
         .into_iter()
         .enumerate()
         .map(|(i, c)| {
-            let i = i % spec.sample_rate as usize;
-            if i > 300 && i < 3400 {
-                c
+            let i = i % sample_rate;
+            const LOW_BOUND: usize = 500;
+            const HIGH_BOUND: usize = 5000;
+            if i > LOW_BOUND && i < HIGH_BOUND
+                || i < sample_rate - LOW_BOUND && i > sample_rate - HIGH_BOUND
+            {
+                // if true {
+                c.unscale(sample_rate as f32 / 2.)
             } else {
                 Complex::new(0f32, 0f32)
             }
         })
         .collect::<Vec<_>>();
 
-    let fft = planner.plan_fft_inverse(spec.sample_rate as usize);
+    let fft = planner.plan_fft_inverse(sample_rate);
     fft.process(&mut samples);
 
-    let samples = samples.into_iter().map(|c| c.re as i16).collect::<Vec<_>>();
+    let samples = samples
+        .into_iter()
+        .map(|c| (c.re / 2.) as i16)
+        .collect::<Vec<_>>();
 
     let mut writer = hound::WavWriter::create("result.wav", spec)?;
     for sample in samples {
